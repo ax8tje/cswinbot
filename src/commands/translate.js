0752e6583c83d8bee41727ae9ccf5f5c8c18d346
ax8tje <131400863+ax8tje@users.noreply.github.com>
2026-02-19 15:08:22 +0100

    translator

---
+++ src/commands/translate.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+module.exports = {
+    name: "translate",
+    description: "Translate text from English to Polish",
+    options: [
+        {
+            name: "text",
+            description: "The English text to translate",
+            type: 3, // STRING
+            required: true
+        }
+    ],
+    run: async (client, interaction) => {
+        const text = interaction.options.getString("text");
+
+        await interaction.deferReply({ ephemeral: true });
+
+        const response = await axios.post(
+            "https://api-free.deepl.com/v2/translate",
+            new URLSearchParams({
+                auth_key: process.env.DEEPL_KEY,
+                text,
+                source_lang: "EN",
+                target_lang: "PL"
+            })
+        );
+
+        const translated = response.data.translations[0].text;
+
+        const webhooks = await interaction.channel.fetchWebhooks();
+        let webhook = webhooks.find(wh => wh.owner?.id === client.user.id);
+
+        if (!webhook) {
+            webhook = await interaction.channel.createWebhook({ name: "Translate" });
+        }
+
+        await webhook.send({
+            content: translated,
+            username: interaction.member.displayName,
+            avatarURL: interaction.user.displayAvatarURL()
+        });
+
+        await interaction.deleteReply();
+    }
+};
